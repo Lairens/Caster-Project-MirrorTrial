e0dab7565ec74a8a028b92534680658cb3306642
Maxence Mognat <m.mognat@gmail.com>
2022-02-05 00:43:18 -1000

    Placing basic building block with player inputs handled by a scene object slaved to the server. Player inputs call commands on the server to execute on the client.

--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -8,32 +8,32 @@ public class PlayerInputHandler : NetworkBehaviour
 {
     public PlayerManager playerManager;
 
-    public override void OnStartClient()
-    {
-        base.OnStartClient();
-        if(!hasAuthority) {return;}
-        NetworkIdentity networkIdentity = NetworkClient.connection.identity;
-        playerManager = networkIdentity.GetComponent<PlayerManager>();
-    }
-    
-
     #region Player Inputs
 
     /****** Send calls to the server to select, or deselect the spells and have it print on the UI passing in the specific spell slot ******/
     public void OnSpell_0(InputAction.CallbackContext context)
     {
+        NetworkIdentity networkIdentity = NetworkClient.connection.identity;
+        playerManager = networkIdentity.GetComponent<PlayerManager>();
+
         if(!context.action.triggered){return;}
         playerManager.CmdToggleSpell(context.action.name);
     }
 
     public void OnSpell_1(InputAction.CallbackContext context)
     {
+        NetworkIdentity networkIdentity = NetworkClient.connection.identity;
+        playerManager = networkIdentity.GetComponent<PlayerManager>();
+
         if(!context.action.triggered){return;}
         playerManager.CmdToggleSpell(context.action.name);
     }
 
     public void OnSpell_2(InputAction.CallbackContext context)
     {
+        NetworkIdentity networkIdentity = NetworkClient.connection.identity;
+        playerManager = networkIdentity.GetComponent<PlayerManager>();
+
         if(!context.action.triggered){return;}
         playerManager.CmdToggleSpell(context.action.name);
     }
@@ -41,6 +41,9 @@ public class PlayerInputHandler : NetworkBehaviour
     /****** Send call to the server to randomise the selected spells ******/
     public void OnRandomizeSpell(InputAction.CallbackContext context)
     {
+        NetworkIdentity networkIdentity = NetworkClient.connection.identity;
+        playerManager = networkIdentity.GetComponent<PlayerManager>();
+
         if(!context.action.triggered){return;}
         playerManager.CmdRandomizeSelectedSpells();
     }
@@ -48,6 +51,9 @@ public class PlayerInputHandler : NetworkBehaviour
     /****** Send call to the server to cast the spell ******/
     public void OnCastSpell(InputAction.CallbackContext context)
     {
+        NetworkIdentity networkIdentity = NetworkClient.connection.identity;
+        playerManager = networkIdentity.GetComponent<PlayerManager>();
+        
         if(!context.action.triggered){return;}
         playerManager.CmdCastSelectedSpell();
     }
